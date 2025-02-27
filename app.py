1601d869c5f0b9cc0f8b1114f43cd11f00b5c931
Thane Chaves <tmoonder@gmail.com>
2025-02-27 18:12:52 -0500

    Added basic database connection with get and post methods.

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return "Hello World"
+
+
+def db_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect("pokemon.sqlite")
+    except sqlite3.Error as e:
+        print("Error while connecting to SQLite database:", e)
+    return conn
+
+
+@app.route("/collection", methods=["GET", "POST"])
+def collection():
+    conn = db_connection()
+    cursor = conn.cursor()
+
+    if request.method == "GET":
+        try:
+            cursor.execute("SELECT * FROM collection")
+            cards = [row for row in cursor.fetchall()]
+            return jsonify(cards), 200
+        except sqlite3.Error as e:
+            return jsonify({"error": str(e)}), 500
+    elif request.method == "POST":
+        try:
+            new_card = request.args.get("card")
+            cursor.execute("INSERT INTO collection (card) VALUES (?)", (new_card))
+            conn.commit()
+            return jsonify({"message": "Card added successfully"}), 201
+        except sqlite3.Error as e:
+            return jsonify({"error": str(e)}, 500)
