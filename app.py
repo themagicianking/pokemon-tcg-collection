8825d42c00047b5191b07c5e98a8a7e573b9aef8
Thane Chaves <tmoonder@gmail.com>
2025-02-28 20:23:39 -0500

    Populated database with set and created endpoint for it.

--- app.py
+++ app.py
@@ -5,31 +5,59 @@ import sqlite3
 app = Flask(__name__)
 
 
+def get_set():
+    try:
+        response = requests.get(
+            "https://api.pokemontcg.io/v2/cards?set.name:HS—Undaunted"
+        )
+        if response.status_code == 200:
+            cards = response.json()["data"]
+            return cards
+        else:
+            return "Error fetching card set."
+    except BaseException:
+        return "Could not connect to API."
+
+
+def create_database():
+    # try:
+    cardset = get_set()
+    conn = sqlite3.connect("pokemon.db")
+    c = conn.cursor()
+    c.execute("""DROP TABLE IF EXISTS cardset""")
+    c.execute(
+        """CREATE TABLE IF NOT EXISTS cardset (id INTEGER PRIMARY KEY, name VARCHAR, type VARCHAR, image VARCHAR)"""
+    )
+    print("Created cardset table")
+    for card in cardset:
+        c.execute(
+            "INSERT INTO cardset (name, type, image) VALUES (?,?,?)",
+            (card["name"], card["types"][0], card["images"]["small"]),
+        )
+    print("Added cards to cardset table")
+    conn.commit()
+    conn.close()
+    # except sqlite3.Error as e:
+    #     print("Error while connecting to SQLite database:", e)
+
+
 @app.route("/")
 def main():
     return render_template("base.html")
 
 
-def db_connection():
-    conn = None
-    try:
-        conn = sqlite3.connect("pokemon.sqlite")
-    except sqlite3.Error as e:
-        print("Error while connecting to SQLite database:", e)
-    return conn
-
-
 @app.route("/collection", methods=["GET", "POST"])
 def collection():
-    conn = db_connection()
+    conn = sqlite3.connect("pokemon.db")
     cursor = conn.cursor()
 
     if request.method == "GET":
         try:
-            return render_template("collection.html")
-            # cursor.execute("SELECT * FROM collection")
-            # cards = [row for row in cursor.fetchall()]
-            # return jsonify(cards), 200
+            cursor.execute("SELECT * FROM cardset")
+            cards = cursor.fetchall()
+            conn.close()
+            return jsonify(cards), 200
+            # return render_template("collection.html", cards=cards)
         except sqlite3.Error as e:
             return jsonify({"error": str(e)}), 500
     elif request.method == "POST":
@@ -37,6 +65,7 @@ def collection():
             new_card = request.args.get("card")
             cursor.execute("INSERT INTO collection (card) VALUES (?)", (new_card))
             conn.commit()
+            conn.close()
             return jsonify({"message": "Card added successfully"}), 201
         except sqlite3.Error as e:
             return jsonify({"error": str(e)}, 500)
@@ -57,3 +86,8 @@ def search():
     except:
         cards = {}
     return render_template("search.html", cards=cards)
+
+
+if __name__ == "__main__":
+    create_database()
+    app.run(debug=True)
