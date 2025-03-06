87ef58362c039afa740fd04be66e5a79ebf10256
Thane Chaves <tmoonder@gmail.com>
2025-03-06 16:19:39 -0500

    Implemented full text search library that utilizes user form input.

--- app.py
+++ app.py
@@ -62,19 +62,22 @@ def main():
             "CREATE VIRTUAL TABLE IF NOT EXISTS cardsearch USING FTS5(id,name,stage,energytype,image,text)"
         )
         c.execute("INSERT INTO cardsearch SELECT * FROM cardset")
+        search = request.form.get("search")
         # stage = request.form.get("stage")
         # energy_type = request.form.get("energy_type")
         # conn = sqlite3.connect("pokemon.db")
         # conn.row_factory = sqlite3.Row
         # c = conn.cursor()
-        c.execute("SELECT * FROM cardsearch WHERE text MATCH 'times'")
+        c.execute("SELECT * FROM cardsearch WHERE cardsearch MATCH (?)", (search,))
         results = [dict(row) for row in c.fetchall()]
+        conn.close()
     else:
         conn = sqlite3.connect("pokemon.db")
         conn.row_factory = sqlite3.Row
         c = conn.cursor()
         c.execute("SELECT * FROM cardset")
         results = [dict(row) for row in c.fetchall()]
+        conn.close()
     return render_template("base.html", results=results)
 
 
